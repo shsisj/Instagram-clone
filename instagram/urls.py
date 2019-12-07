0b98000e234f0b6d4c1852ac580aa973d40c43a2
jeff ubayi <jeffubayi@gmail.com>
2019-12-07 10:30:56 +0300

    url mapping

--- instagram/urls.py
+++ instagram/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('clone.urls')),
     url(r'^accounts/',include('registration.backends.simple.urls')),
-    #url(r'^logout/$',views.logout, {"next_page":'/'},name="logout"),
+    url(r'^logout/$',views.logout, {"next_page":'/'},name="logout"),
     url(r'^tinymce',include('tinymce.urls')),
 ]
